5c0c02434021b0226dd7a4f4b0d5cf5e98278edf
Amir Taaki <amir@dyne.org>
2019-11-27 17:12:32 +0100

    correct server string for testnet

--- config.hpp
+++ config.hpp
@@ -4,8 +4,8 @@
 constexpr auto is_testnet = true;
 
 constexpr auto blockchain_server_address = is_testnet ?
-    "tcp://testnet.libbitcoin.net:9091" :
-    "tcp://testnet.libbitcoin.net:9091";
+    "tcp://testnet1.libbitcoin.net:19091" :
+    "tcp://mainnet.libbitcoin.net:9091";
 
 #endif // BIJIWALLET_CONFIG_HPP
 
